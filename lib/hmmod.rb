2a72936b62788f59ae3938853b99cea8bfa137ad
msmar9 <miquelsaezmartin@gmail.com>
2022-10-17 13:48:46 +0200

    Write save game func (relative path file)

--- lib/hmmod.rb
+++ lib/hmmod.rb
@@ -1,5 +1,5 @@
 module HMMod
-  WORD_LIST = File.read('./google-10000-english-no-swears.txt').split("\n").select {|word| word.length.between?(5,12)}
+  WORD_LIST = File.read('/home/miquel/repos/hangman/lib/google-10000-english-no-swears.txt').split("\n").select {|word| word.length.between?(5,12)}
 
   def valid_input?(input)
     # Checks if the input is a single character of the english alphabet
@@ -15,6 +15,17 @@ module HMMod
     end
     input = valid_input?(input = gets.chomp) ? input : ask_again
   end
+
+  def save_game(save_name, game_data)
+    # Game data has the structure W O R D | g u e s s e s
+    address = '../saves/' + save_name + '.txt'
+    if File.exist?(address)
+      File.delete(address)
+    end
+    save_file = File.open(address,"w")
+    save_file.puts game_data
+    save_file.close
+  end
 end
 
 # Testing
@@ -22,4 +33,7 @@ include HMMod
 #p WORD_LIST[0,21]
 #p valid_input?('r')
 #p valid_input?('5')
-p ask_input
+#p ask_input
+#save_game('test', 'T _ S T S | a i o u')
+#tst = File.open('./../saves/test.txt',"r")
+#puts tst.read
